6e6ff76b3fb1cee79050728240701025478d169b
KlorPe <z76196702@gmail.com>
2025-01-12 15:38:50 +0200

    Update menu.lua

--- src/menu.lua
+++ src/menu.lua
@@ -2808,7 +2808,7 @@ function miniFling(playerToFling)
 end
 
 local NonameTab = Window:MakeTab({ 
-    Name = "Індивідуальні      скрипти", 
+    Name = "Індивідуальні   /   скрипти", 
     Icon = "rbxassetid://17404114716",
     PremiumOnly = false
 })
